c4505a6223c4af16c5646f5c5f4368caaade265f
Nitin Bodke <nitin.bodke@broadcom.com>
2021-07-10 12:58:26 +0530

    Added Application Logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s : %(message)s')
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    logging.debug("%s endpoint was reached", request.endpoint)
     return "Hello World!"
 
 
 @app.route("/status")
 def status():
+    logging.debug("%s endpoint was reached", request.endpoint)
     return {'result': 'OK - healthy'}
 
 
 @app.route("/metrics")
 def metrics():
+    logging.debug("%s endpoint was reached", request.endpoint)
     return {'data': {'UserCount': 140, 'UserCountActive': 23}}
 
 
